dcd04484ac15e8e121972b67b0b1b7ba348e5b9e
Dmitry Stupin <dm.stup@mail.ru>
2024-04-12 12:45:53 +0700

    Создание анкеты
    Доверстал анкету магазина. Не закончил только работу над выпадающим списком "Род деятельности".
    Прописал скрипты, которые блокируют написание символов в поля, которые им не подходят

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/* Запрет писать цифры в input */
+function noDigits(event) {
+    if ("1234567890~`'[]{}|!@#$%^&*()_+№;%:?/\"=\\".indexOf(event.key) != -1)
+      event.preventDefault();
+    }
+
+let tel = document.querySelector('[type=tel]');
+
+let reg = /[A-Za-zА-Яа-яЁё]/g;
+let reg1 = /[A-Za-z]/g;
+let reg2 = /[e.]/g;
+
+tel.oninput = function(){
+    this.value = this.value.replace(reg, '')
+}
+fullname.oninput = function(){
+    this.value = this.value.replace(reg1, '')
+}
+age.oninput = function(){
+    this.value = this.value.replace(reg2, '');
+    this.value = this.value.slice(0, 2);
+}
