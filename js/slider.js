86bb8512edb45c893dcdec2aff2e38f125401aa5
Dmitry Stupin <dm.stup@mail.ru>
2024-04-15 16:31:18 +0700

    15.04.2024
    Верстка страницы товаров, исправления багов

--- js/slider.js
+++ js/slider.js
@@ -2,9 +2,9 @@ $(function () {
     $('.photo__slider').slick({
         centerMode: true,
         infinite: true,
-        centerPadding: '0px',
-        arrows: false,
+        arrows: true,
         slidesToShow: 3,
-        appendArrows: $('.photo__slider-arrows'),
+        variableWidth: true,
+        adaptiveHeight: true
     })
 })
