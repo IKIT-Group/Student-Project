6ec08c4da9bf83827dc430bbe43627579f06ba79
Dmitry Stupin <dm.stup@mail.ru>
2024-05-01 14:25:32 +0700

    #62 (01.05.2024)
    Сделана адаптация формы заполнения анкеты. На ней же было добавлено модальное окно, при нажатии на кнопку "Отправить заявку".

---
+++ js/dialog.js
@@ -0,0 +1,17 @@
+let dialogOpener = document.querySelector('.form__button');
+let dialog = document.querySelector('.form__modal');
+let inputForm = document.querySelectorAll('input')
+
+
+
+dialogOpener.addEventListener("click", openModalAndLockScroll)
+dialog.addEventListener("close", returnScroll)
+
+function openModalAndLockScroll() {
+  dialog.showModal()
+  document.body.classList.add("scroll-lock")
+}
+
+function returnScroll() {
+  document.body.classList.remove("scroll-lock")
+}
